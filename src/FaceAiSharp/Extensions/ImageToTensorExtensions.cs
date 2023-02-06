addd9a3714f473d4aad2ffb07883d79e3567976b
Georg Jung <git@gjung.com>
2023-02-06 14:32:34 +0100

    Improve optimized ImageToTensor

--- src/FaceAiSharp/Extensions/ImageToTensorExtensions.cs
+++ src/FaceAiSharp/Extensions/ImageToTensorExtensions.cs
@@ -10,34 +10,36 @@ namespace FaceAiSharp.Extensions;
 
 public static class ImageToTensorExtensions
 {
-    // this is heavily based on https://github.com/skywalkerisnull/onnxruntime-csharp-cv-template/blob/bb1454a51a722e293a918d2b5a25abda864f9f74/utils/ImageHelper.cs#L62
-    // see also https://github.com/SixLabors/ImageSharp/discussions/1955
+    /* this is heavily based on https://github.com/skywalkerisnull/onnxruntime-csharp-cv-template/blob/bb1454a51a722e293a918d2b5a25abda864f9f74/utils/ImageHelper.cs#L62
+     * see also https://github.com/SixLabors/ImageSharp/discussions/1955
+     */
+
+    public static DenseTensor<float> ToTensor(this Image<Rgb24> image)
+    {
+        var mean = new[] { 0.5f, 0.5f, 0.5f };
+        var stddev = new[] { 1f, 1f, 1f };
+        var dims = new[] { 1, 3, image.Height, image.Width };
+        return ImageToTensor(new[] { image }, mean, stddev, dims);
+    }
 
     public static DenseTensor<float> ToTensor(this Image<Rgb24> image, float[] mean, float[] stddev, int[] inputDimension)
     {
-        return ImageToTensor(new[] { image }, mean, stddev, inputDimension).First();
+        return ImageToTensor(new[] { image }, mean, stddev, inputDimension);
     }
 
     /// <summary>
-    /// Converts the list of images into batches and list of input tensors.
+    /// Efficiently converts images to a input tensor for batch processing.
     /// </summary>
-    /// <param name="images"></param>
-    /// <param name="mean"></param>
-    /// <param name="stddev"></param>
+    /// <param name="images">The images to convert.</param>
+    /// <param name="mean">The rgb mean values used for normalization.</param>
+    /// <param name="stddev">The rgb stddev values used for normalization.</param>
     /// <param name="inputDimension">The size of the tensor that the OnnxRuntime model is expecting, e.g. [1, 3, 224, 224].</param>
-    public static List<DenseTensor<float>> ImageToTensor(IReadOnlyList<Image<Rgb24>> images, float[] mean, float[] stddev, int[] inputDimension)
+    /// <returns>A tensor that contains the converted batch of images.</returns>
+    public static DenseTensor<float> ImageToTensor(IReadOnlyCollection<Image<Rgb24>> images, float[] mean, float[] stddev, int[] inputDimension)
     {
-        // Used to create more than one batch
-        int numberBatches = 1;
-
-        // If required, can create batches of different sizes
-        var batchSizes = new int[] { images.Count };
-
         var strides = GetStrides(inputDimension);
 
-        var inputs = new List<DenseTensor<float>>();
-
-        // Faster normalisation process
+        // Calculate these outside the loop
         var normR = mean[0] / stddev[0];
         var normG = mean[1] / stddev[1];
         var normB = mean[2] / stddev[2];
@@ -46,50 +48,39 @@ public static class ImageToTensorExtensions
         var stdNormG = 1 / (255f * stddev[1]);
         var stdNormB = 1 / (255f * stddev[2]);
 
-        for (var j = 0; j < numberBatches; j++)
+        inputDimension[0] = images.Count;
+
+        var input = new DenseTensor<float>(inputDimension);
+
+        foreach (var image in images)
         {
-            inputDimension[0] = batchSizes[j];
-
-            // Need to directly use a DenseTensor here because we need access to the underlying span.
-            DenseTensor<float> input = new DenseTensor<float>(inputDimension);
-
-            for (var i = 0; i < batchSizes[j]; i++)
+            image.ProcessPixelRows(pixelAccessor =>
             {
-                var image = images[i];
-                var index = 0;
-
-                image.ProcessPixelRows(pixelAccessor =>
+                var inputSpan = input.Buffer.Span;
+                for (var y = 0; y < image.Height; y++)
                 {
-                    var inputSpan = input.Buffer.Span;
-                    for (var y = 0; y < image.Height; y++)
+                    var index = y * strides[2];
+                    var rowSpan = pixelAccessor.GetRowSpan(y);
+
+                    // Faster indexing into the span
+                    var spanR = inputSpan.Slice(index, rowSpan.Length);
+                    index += strides[1];
+                    var spanG = inputSpan.Slice(index, rowSpan.Length);
+                    index += strides[1];
+                    var spanB = inputSpan.Slice(index, rowSpan.Length);
+
+                    // Now we can just directly loop through and copy the values directly from span to span.
+                    for (int x = 0; x < rowSpan.Length; x++)
                     {
-                        index = y * strides[2];
-
-                        var rowSpan = pixelAccessor.GetRowSpan(y);
-
-                        // Faster indexing into the span
-                        var spanR = inputSpan.Slice(index, image.Width);
-                        index += strides[1];
-                        var spanG = inputSpan.Slice(index, image.Width);
-                        index += strides[1];
-                        var spanB = inputSpan.Slice(index, image.Width);
-
-                        // Now we can just directly loop through and copy the values directly from span to span.
-                        for (int x = 0; x < image.Width; x++)
-                        {
-                            spanR[x] = (rowSpan[x].R * stdNormR) - normR;
-                            spanG[x] = (rowSpan[x].G * stdNormG) - normG;
-                            spanB[x] = (rowSpan[x].B * stdNormB) - normB;
-                        }
-
+                        spanR[x] = (rowSpan[x].R * stdNormR) - normR;
+                        spanG[x] = (rowSpan[x].G * stdNormG) - normG;
+                        spanB[x] = (rowSpan[x].B * stdNormB) - normB;
                     }
-                });
-
-                inputs.Add(input);
-            }
+                }
+            });
         }
 
-        return inputs;
+        return input;
     }
 
     /// <summary>
@@ -98,7 +89,6 @@ public static class ImageToTensorExtensions
     private static int[] GetStrides(ReadOnlySpan<int> dimensions, bool reverseStride = false)
     {
         int[] strides = new int[dimensions.Length];
-
         if (dimensions.Length == 0)
         {
             return strides;
@@ -123,22 +113,5 @@ public static class ImageToTensorExtensions
         }
 
         return strides;
-    }
-
-
-    /// <summary>
-    /// Calculates the 1-d index for n-d indices in layout specified by strides.
-    /// </summary>
-    private static int GetIndex(int[] strides, ReadOnlySpan<int> indices, int startFromDimension = 0)
-    {
-        Debug.Assert(strides.Length == indices.Length, "strides.Length must equal indices.Length");
-
-        int index = 0;
-        for (int i = startFromDimension; i < indices.Length; i++)
-        {
-            index += strides[i] * indices[i];
-        }
-
-        return index;
     }
 }
