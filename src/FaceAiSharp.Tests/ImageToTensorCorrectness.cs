255f550510cce6f13f0d97e5c7e164b5808c3a8f
Georg Jung <git@gjung.com>
2023-02-06 14:39:36 +0100

    Add test for optimized ImageToTensor implementation

---
+++ src/FaceAiSharp.Tests/ImageToTensorCorrectness.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Georg Jung. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FaceAiSharp.Extensions;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FaceAiSharp.Tests;
+
+public class ImageToTensorCorrectness
+{
+    private readonly string _imgPath = $@"{AppContext.BaseDirectory}..\..\..\..\media\portraits\Barack_Obama_01.jpg";
+    private readonly Image<Rgb24> _img;
+
+    public ImageToTensorCorrectness()
+    {
+        _img = Image.Load<Rgb24>(_imgPath);
+    }
+
+    [Fact]
+    public void OptimizedEqualsNaive()
+    {
+        var optimized = _img.ToTensor();
+        var naive = Naive(_img);
+
+        var optimizedBuffer = optimized.Buffer.Span;
+        var naiveBuffer = naive.Buffer.Span;
+        Assert.Equal(naiveBuffer.Length, optimizedBuffer.Length);
+
+        for (var i = 0; i < naiveBuffer.Length; i++)
+        {
+            Assert.Equal(naiveBuffer[i], optimizedBuffer[i], 0.00001f);
+        }
+    }
+
+    internal static DenseTensor<float> Naive(Image<Rgb24> img)
+    {
+        var ret = new DenseTensor<float>(new[] { 1, 3, img.Height, img.Width });
+
+        var mean = new[] { 0.5f, 0.5f, 0.5f };
+        img.ProcessPixelRows(accessor =>
+        {
+            for (var y = 0; y < accessor.Height; y++)
+            {
+                Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
+                for (var x = 0; x < accessor.Width; x++)
+                {
+                    ret[0, 0, y, x] = (pixelSpan[x].R / 255f) - mean[0];
+                    ret[0, 1, y, x] = (pixelSpan[x].G / 255f) - mean[1];
+                    ret[0, 2, y, x] = (pixelSpan[x].B / 255f) - mean[2];
+                }
+            }
+        });
+
+        return ret;
+    }
+}
