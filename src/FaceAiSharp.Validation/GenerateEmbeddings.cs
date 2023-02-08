def15796b217ff99a077b332478170d8957770d9
Georg Jung <git@gjung.com>
2023-02-08 14:22:39 +0100

    Use System.CommandLine for validation

---
+++ src/FaceAiSharp.Validation/GenerateEmbeddings.cs
@@ -0,0 +1,125 @@
+// Copyright (c) Georg Jung. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine;
+using System.Data;
+using System.Diagnostics;
+using System.Threading.Channels;
+using FaceAiSharp.Abstractions;
+using FaceAiSharp.Extensions;
+using LiteDB;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Microsoft.ML.OnnxRuntime;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FaceAiSharp.Validation;
+
+internal sealed class GenerateEmbeddings : IDisposable
+{
+    private readonly SessionOptions _embedderSessOpts;
+    private readonly IFaceEmbeddingsGenerator _emb;
+    private readonly IFaceDetector _det;
+    private readonly IMemoryCache _cache;
+    private readonly DirectoryInfo _dataset;
+    private readonly FileInfo _db;
+
+    public GenerateEmbeddings(DirectoryInfo dataset, FileInfo db, FileInfo arcFaceModel, FileInfo scrfdModel)
+    {
+        _embedderSessOpts = new SessionOptions
+        {
+            EnableMemoryPattern = false,
+        };
+        _embedderSessOpts.AppendExecutionProvider_DML();
+
+        _emb = new ArcFaceEmbeddingsGenerator(
+            new()
+            {
+                ModelPath = arcFaceModel.FullName,
+            },
+            _embedderSessOpts);
+
+        var opts = new MemoryCacheOptions();
+        var iopts = Options.Create(opts);
+        _cache = new MemoryCache(iopts);
+
+        _det = new ScrfdDetector(
+            new()
+            {
+                ModelPath = scrfdModel.FullName,
+            },
+            _cache);
+
+        _dataset = dataset;
+        _db = db;
+    }
+
+    public async Task Invoke()
+    {
+        using var db = new LiteDatabase(_db.FullName);
+        var dbEmb = db.GetCollection<EmbedderResult>();
+        dbEmb.EnsureIndex(x => x.FilePath);
+
+        var setFolder = _dataset.FullName;
+
+        var setEnum = DatasetIterator.EnumerateFolderPerIdentity(setFolder).Where(x => !dbEmb.Exists(db => db.FilePath == x.FilePath));
+        /* var embeddings = DatasetIterator.EnumerateEmbedderResults(emb, Preprocess, setEnum); */
+
+        var ch = Channel.CreateBounded<ChannelData>(10);
+        var producerTask = Task.Run(() => ProducePreprocessed(setEnum, ch));
+        var embeddings = GenerateEmbeddingsFromChannel(ch);
+
+        await foreach (var (embRes, ticks) in embeddings)
+        {
+            dbEmb.Insert(embRes);
+            Console.WriteLine($"{ticks,4:D}ms : {embRes.FilePath}");
+            db.Commit();
+        }
+
+        await producerTask;
+    }
+
+    public void Dispose()
+    {
+        _embedderSessOpts.Dispose();
+        (_emb as IDisposable)?.Dispose();
+        (_det as IDisposable)?.Dispose();
+        _cache.Dispose();
+    }
+
+    private Image<Rgb24> Preprocess(string filePath)
+    {
+        var img = Image.Load<Rgb24>(filePath);
+        var x = _det.Detect(img);
+        var first = x.First();
+        Debug.Assert(first.Landmarks != null, "No landmarks detected but required");
+        var angle = ScrfdDetector.GetFaceAlignmentAngle(first.Landmarks);
+        img.CropAlignedDestructive(Rectangle.Round(first.Box), (float)angle);
+        return img;
+    }
+
+    private async Task ProducePreprocessed(IEnumerable<DatasetImage> images, ChannelWriter<ChannelData> channel)
+    {
+        await Parallel.ForEachAsync(images, async (DatasetImage image, CancellationToken tok) =>
+        {
+            var sw = Stopwatch.StartNew();
+            var img = Preprocess(image.FilePath);
+            await channel.WriteAsync(new(image, img, sw.ElapsedMilliseconds));
+        });
+
+        channel.Complete();
+    }
+
+    private async IAsyncEnumerable<(EmbedderResult Result, long Ticks)> GenerateEmbeddingsFromChannel(ChannelReader<ChannelData> channel)
+    {
+        await foreach (var (metadata, img, ticks) in channel.ReadAllAsync())
+        {
+            var sw = Stopwatch.StartNew();
+            var embedding = _emb.Generate(img);
+            yield return (new(metadata.Identity, metadata.FilePath, embedding), ticks + sw.ElapsedMilliseconds);
+        }
+    }
+
+    internal readonly record struct ChannelData(DatasetImage Metadata, Image<Rgb24> Image, long ElapsedTicks);
+}
--- src/FaceAiSharp.Validation/Program.cs
+++ src/FaceAiSharp.Validation/Program.cs
@@ -1,96 +1,39 @@
 // Copyright (c) Georg Jung. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Data.Common;
-using System.Diagnostics;
-using System.Threading.Channels;
-using FaceAiSharp;
-using FaceAiSharp.Abstractions;
-using FaceAiSharp.Extensions;
+using System.CommandLine;
 using FaceAiSharp.Validation;
-using LiteDB;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Options;
-using Microsoft.ML.OnnxRuntime;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
 
-var sessOpts = new SessionOptions();
-sessOpts.EnableMemoryPattern = false;
-sessOpts.AppendExecutionProvider_DML();
+var rc = new RootCommand("FaceAiSharp validation tools");
 
-var emb = new ArcFaceEmbeddingsGenerator(
-    new()
-    {
-        ModelPath = @"C:\Users\georg\facePics\arcfaceresnet100-8\resnet100\resnet100.onnx",
-    },
-    sessOpts);
+var db = new Option<FileInfo>(
+    name: "--db",
+    description: "File to use as db to store results and for continuation",
+    getDefaultValue: () => new FileInfo("faceaisharp-validation.litedb"));
+rc.AddGlobalOption(db);
 
-var opts = new MemoryCacheOptions();
-var iopts = Options.Create(opts);
-var cache = new MemoryCache(iopts);
+var dataset = new Option<DirectoryInfo>(
+    name: "--dataset",
+    getDefaultValue: () => new DirectoryInfo(@"C:\Users\georg\Downloads\lfw\lfw"));
 
-var det = new ScrfdDetector(
-    new()
-    {
-        ModelPath = @"C:\Users\georg\OneDrive\Dokumente\ScrfdOnnx\scrfd_2.5g_bnkps.onnx",
-    },
-    cache);
+var arcfaceModel = new Option<FileInfo>(
+    name: "--arcface-model",
+    getDefaultValue: () => new FileInfo(@"C:\Users\georg\facePics\arcfaceresnet100-8\resnet100\resnet100.onnx"));
 
-using var db = new LiteDatabase("embeddings.litedb");
-var dbEmb = db.GetCollection<EmbedderResult>();
-dbEmb.EnsureIndex(x => x.FilePath);
+var scrfdModel = new Option<FileInfo>(
+    name: "--scrfd-model",
+    getDefaultValue: () => new FileInfo(@"C:\Users\georg\OneDrive\Dokumente\ScrfdOnnx\scrfd_2.5g_bnkps.onnx"));
 
-var setFolder = @"C:\Users\georg\Downloads\lfw\lfw";
+var generateEmbeddings = new Command("generate-embeddings") { dataset, arcfaceModel, scrfdModel };
 
-var setEnum = DatasetIterator.EnumerateFolderPerIdentity(setFolder).Where(x => !dbEmb.Exists(db => db.FilePath == x.FilePath));
-/* var embeddings = DatasetIterator.EnumerateEmbedderResults(emb, Preprocess, setEnum); */
+#pragma warning disable SA1116 // Split parameters should start on line after declaration
+#pragma warning disable SA1117 // Parameters should be on same line or separate lines
 
-var ch = Channel.CreateBounded<ChannelData>(10);
-var producerTask = ProducePreprocessed(setEnum, ch);
-var embeddings = GenerateEmbeddingsFromChannel(emb, ch.Reader);
-
-await foreach (var (embRes, ticks) in embeddings)
+generateEmbeddings.SetHandler(async (dataset, db, arcfaceModel, scrfdModel) =>
 {
-    dbEmb.Insert(embRes);
-    Console.WriteLine($"{ticks,4:D}ms : {embRes.FilePath}");
-    db.Commit();
-}
+    var cmd = new GenerateEmbeddings(dataset, db, arcfaceModel, scrfdModel);
+    await cmd.Invoke();
+}, dataset, db, arcfaceModel, scrfdModel);
+rc.AddCommand(generateEmbeddings);
 
-await producerTask;
-
-Image<Rgb24> Preprocess(string filePath)
-{
-    var img = Image.Load<Rgb24>(filePath);
-    var x = det.Detect(img);
-    var first = x.First();
-    Debug.Assert(first.Landmarks != null, "No landmarks detected but required");
-    var angle = ScrfdDetector.GetFaceAlignmentAngle(first.Landmarks);
-    img.CropAlignedDestructive(Rectangle.Round(first.Box), (float)angle);
-    return img;
-}
-
-async Task ProducePreprocessed(IEnumerable<DatasetImage> images, ChannelWriter<ChannelData> channel)
-{
-    await Parallel.ForEachAsync(images, async (DatasetImage image, CancellationToken tok) =>
-    {
-        var sw = Stopwatch.StartNew();
-        var img = Preprocess(image.FilePath);
-        await channel.WriteAsync(new(image, img, sw.ElapsedMilliseconds));
-    });
-
-    channel.Complete();
-}
-
-static async IAsyncEnumerable<(EmbedderResult, long Ticks)> GenerateEmbeddingsFromChannel(IFaceEmbeddingsGenerator embedder, ChannelReader<ChannelData> channel)
-{
-    await foreach (var (metadata, img, ticks) in channel.ReadAllAsync())
-    {
-        var sw = Stopwatch.StartNew();
-        var embedding = embedder.Generate(img);
-        yield return (new(metadata.Identity, metadata.FilePath, embedding), ticks + sw.ElapsedMilliseconds);
-    }
-}
-
-#pragma warning disable SA1649 // File name should match first type name
-internal readonly record struct ChannelData(DatasetImage Metadata, Image<Rgb24> Image, long ElapsedTicks);
+return await rc.InvokeAsync(args);
