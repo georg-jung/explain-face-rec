55070f6f11d6e97262d6744b5ce490a258c11816
Georg Jung <git@gjung.com>
2023-03-06 19:27:10 +0100

    Improve blur faces

--- src/FaceAiSharp/Applications.cs
+++ src/FaceAiSharp/Applications.cs
@@ -10,9 +10,17 @@ namespace FaceAiSharp;
 
 public static class Applications
 {
-    public static void BlurFaces(this IFaceDetector detector, Image<Rgb24> input, float blurSigmaFactor = 10f)
+    /// <summary>
+    /// Blurs all faces that are found by the given face detector. Modifies the given image (destructive).
+    /// </summary>
+    /// <param name="detector">The face detector to use.</param>
+    /// <param name="input">The image to search and blur faces in.</param>
+    /// <param name="blurSigmaFactor">Factor to determine sigma for the gaussian blur. sigma = max(height, width) / factor.</param>
+    /// <returns>The number of faces that have been blurred.</returns>
+    public static int BlurFaces(this IFaceDetector detector, Image<Rgb24> input, float blurSigmaFactor = 10f)
     {
         var res = detector.Detect(input);
+        var cnt = 0;
         input.Mutate(op =>
         {
             foreach (var fc in res)
@@ -22,7 +30,9 @@ public static class Applications
                 var max = Math.Max(r.Width, r.Height);
                 var sigma = Math.Max(max / blurSigmaFactor, blurSigmaFactor);
                 op.GaussianBlur(sigma, r);
+                cnt++;
             }
         });
+        return cnt;
     }
 }
