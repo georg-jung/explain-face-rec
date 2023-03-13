7af55d52ddca90c2152617c80fb91d733132057e
Georg Jung <git@gjung.com>
2023-03-13 17:28:57 +0100

    Add example images

--- src/BlazorFace/Media.cs
+++ src/BlazorFace/Media.cs
@@ -8,12 +8,20 @@ namespace BlazorFace;
 public static class Media
 {
 #if DEBUG
-    public static readonly string PortraitsDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "wwwroot", "media", "portraits");
+    public static readonly string MediaDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "wwwroot", "media");
 #else
-    public static readonly string PortraitsDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "media", "portraits");
+    public static readonly string MediaDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "media");
 #endif
+
+    public static readonly string PortraitsDir = Path.Combine(MediaDir, "portraits");
+
+    public static readonly string GroupsDir = Path.Combine(MediaDir, "groups");
 
     private static readonly Lazy<IReadOnlyList<string>> _portraits = new(() => Directory.GetFiles(PortraitsDir, "*.jpg"));
 
+    private static readonly Lazy<IReadOnlyList<string>> _groups = new(() => Directory.GetFiles(GroupsDir, "*.jpg"));
+
     public static IReadOnlyList<string> Portraits => _portraits.Value;
+
+    public static IReadOnlyList<string> Groups => _groups.Value;
 }
